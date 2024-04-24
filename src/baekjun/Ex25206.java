0ac652daa2b16c21bcc83fc7e870f780c83d529e
USER <USER@1.1.1.162>
2024-04-25 08:44:34 +0900

    push and commit

---
+++ src/baekjun/Ex25206.java
@@ -0,0 +1,55 @@
+package baekjun;
+
+import java.util.Scanner;
+
+public class Ex25206 {
+	public static void main(String[] arg) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double result = 0;
+		double sum = 0;
+		char c;
+		
+		for(int i=0;i<20;i++) {
+			double credit = 0;
+			double grade = 0; 
+			
+			String str_arr[] = scan.nextLine().split(" ");
+			credit += Double.parseDouble(str_arr[1]);
+			c = str_arr[2].charAt(0);
+			
+			switch(c) {
+			case 'A':
+				if(str_arr[2].charAt(1) == '+') grade += 4.5;
+				else grade += 4.0;
+				break;
+			case 'B':
+				if(str_arr[2].charAt(1) == '+') grade += 3.5;
+				else grade += 3.0;
+				break;
+			case 'C':
+				if(str_arr[2].charAt(1) == '+') grade += 2.5;
+				else grade += 2.0;
+				break;
+			case 'D':
+				if(str_arr[2].charAt(1) == '+') grade += 1.5;
+				else grade += 1.0;
+				break;
+			case 'F':
+				grade += 0;
+				break;
+			case 'P':
+				grade += 0;
+				credit = 0;
+				break;
+			}
+			sum += credit;
+			result += credit*grade;
+		}
+		scan.close();
+
+		System.out.println(result/sum);
+		
+	}
+}
