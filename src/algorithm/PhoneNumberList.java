6015a050b7785ddff0b11bb53dd542293076a6c9
donggyun-go <ehdrbsr43@naver.com>
2024-05-07 08:46:58 +0900

    commit

--- src/algorithm/PhoneNumberList.java
+++ src/algorithm/PhoneNumberList.java
@@ -1,5 +1,21 @@
 package algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneNumberList {
+    public boolean solution(String[] phoneBook) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < phoneBook.length; i++) 
+            map.put(phoneBook[i], i);
+        
+        for (int i = 0; i < phoneBook.length; i++)
+            for (int j = 0; j < phoneBook[i].length(); j++)
+                if (map.containsKey(phoneBook[i].substring(0, j)))
+                    return false;
+
+        return true;
+    }
 
 }
