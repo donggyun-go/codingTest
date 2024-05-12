61c926fd84b6723b67240a5a5c4b0be7ebe2c10d
donggyun-go <ehdrbsr43@naver.com>
2024-05-13 08:50:29 +0900

    club99 commit

---
+++ src/club99/Ex14681.java
@@ -0,0 +1,22 @@
+package club99;
+
+import java.util.Scanner;
+
+public class Ex14681 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		scan.close();
+		if(x>0 && y>0) {
+			System.out.println("1");
+		}else if(x<0 && y>0) {
+			System.out.println("2");
+		}else if(x<0 && y<0) {
+			System.out.println("3");
+		}else {
+			System.out.println("4");
+		}
+
+	}
+}
