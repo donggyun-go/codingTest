0d4c522833f1f608be0ee9ee198f969f1a6b15ea
donggyun-go <ehdrbsr43@naver.com>
2024-05-17 11:11:13 +0900

    Ex2869 commit

---
+++ src/club99/Ex2869.java
@@ -0,0 +1,21 @@
+package club99;
+
+import java.util.Scanner;
+
+public class Ex2869 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int V = scan.nextInt();
+        scan.close();
+        
+        int days = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0) {
+            days++;
+        }
+        
+        System.out.println(days);
+    }
+}
+
